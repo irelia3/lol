c395fc5b995ca4a9d389aaa58e96329b4d258661
irelia3 <>
2023-10-02 15:34:29 +0200

    Added game.py

---
+++ src/game.py
@@ -0,0 +1,14 @@
+blue = ["TOPB", "JNGB", "MIDB", "BOTB", "SUPB"]
+red = ["TOPR", "JNGR", "MIDR", "BOTR", "SUPR"]
+
+# Lists matchup pairs, 1v1 and XvX for jungler + lanes. "blue" and "red" arguments are arrays.
+
+def list_matchups(blue, red):
+
+    # This returns an "iterator" of tuples
+    onevs = list(zip(blue, red))
+
+    for x in onevs:
+        print(x)
+
+list_matchups(blue, red)
